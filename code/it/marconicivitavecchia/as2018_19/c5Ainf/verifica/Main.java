4a40c97d8c9e0002e18d8be1c019209e23fa52bb
Your Name <you@example.com>
2018-11-17 11:15:07 +0100

    consegna compito

--- code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
@@ -1,36 +1,98 @@
 
-// TODO Inserire il package corretto
-// nota: potete usare il suggerimento della lampadina a sinistra (scegliete l'unica opzione sensata). 
-package __TODO__;
+//package corretto
+ 
+package it.marconicivitavecchia.as2018_19.c5Ainf.verifica;
 
 import javax.swing.*;
 
 public class Main extends JFrame {
 	
 	public Main() {
-		super("Verifica in laboratorio");
+		
+		super("SpeedTest");
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		
-		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
-		
-		// TODO Usare StringBuilder per costruire la stringa HTML
-		
-		// TODO Creare la stringa HTML per il wireframe
-		
-		// TODO Convertire lo StringBuilder in String
-		
-		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("<html>");
+		sb.append("<h1>SpeedTest</h1>");
+
+		// Create div for table
+		sb.append("<div style='display:table'>");
+
+		// create the first row
+		sb.append("<div style='display:table-row'>");
+
+		// create the date cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("<b>Date</b>");
+		sb.append("</div>");
+		// create the up(mbps) cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("<b>up(Mbps)</b>");
+		sb.append("</div>");
+		// create the down(mbps) cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("<b>down(Mbps)</b>");
+		sb.append("</div>");
+
+		// close first row
+		sb.append("</div>");
+
+		// create the second row
+		sb.append("<div style='display:table-row'>");
+
+		// create the Date cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("1/7/18");
+		sb.append("</div>");
+		// create the up(Mbps) cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("25.0");
+		sb.append("</div>");
+		// create the down(mbps) cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("28.9");
+		sb.append("</div>");
+
+		// close second row
+		sb.append("</div>");
+
+		// create the third row
+		sb.append("<div style='display:table-row'>");
+
+		// create the Date cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("7/9/18");
+		sb.append("</div>");
+		// create the up(Mbps) cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("24.6");
+		sb.append("</div>");
+		// create the down(mbps) cell
+		sb.append("<div style='display:table-cell'>");
+		sb.append("1.99");
+		sb.append("</div>");
+
+		// close third row
+		sb.append("</div>");
+
+		// close div for table
+		sb.append("</div>");
+
+		sb.append("</html>");
+
+		String str = sb.toString();
+
+		super.add(new JLabel(str));
+
+		super.setSize(300,500);
 		super.setVisible(true);
-		 
-		System.out.println("Verifica Swing-GitHub");
-		
-		
+
+		System.out.println("Monolithics application from 5Ainf, TEST");
 	}
-	
-	public static void main (String[] args) {
-		new Main();
+
+	public static void main(String[] args) {
+		Main test = new Main();
 	}
 }
 
